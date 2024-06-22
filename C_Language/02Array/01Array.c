f54a3eff3c78c8271c96190c938601b20224f901
RWn-ChetanSharma <rwa5.chetan.sha@gmail.com>
2024-06-22 15:34:00 +0530

    Lecture Data

---
+++ C_Language/02Array/01Array.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+// Array  => It is a collection of many values with same data type.
+
+// 1D Array 
+
+int main(){
+	int i;
+	int arr[] = {11,22,33,44,55,66,77,88,99};
+			// 	   0, 1, 2, 3, 4
+//	    printf("%d\n", arr[0]);
+//		printf("%d\n", arr[1]);
+//		printf("%d\n", arr[2]);
+//		printf("%d\n", arr[3]);
+//		printf("%d\n", arr[4]);
+
+	for(i=0;i<=8;i++){
+		if(arr[i]%2==0){
+		printf("%d\n", arr[i]);
+		}
+	}
+}
