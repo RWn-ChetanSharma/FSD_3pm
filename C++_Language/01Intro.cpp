4cd2e97fd8caa0cebaec2536f9461022783277ff
RWn-ChetanSharma <rwa5.chetan.sha@gmail.com>
2024-07-25 14:34:45 +0530

    Lecture Data

---
+++ C++_Language/01Intro.cpp
@@ -0,0 +1,33 @@
+// C Languge   => printf("Message");
+
+// C++ Languge => cout << "Message";         // cout => object  // << insertion Operator
+
+// we can use \n as well as << endl for next line
+
+// cout << "Hello" << endl << "World!";            // << endl => Manipulator
+
+// c => printf("Value of a is %d", a);
+
+// c++ => cout << "Value of a is " << a;
+
+//     => cout << a;
+//     => cout << a << "" << b << "--" << c;    // 3 5--8
+//     => cout << a+b;
+
+// c => printf("Sum of %d and %d is %d", a,b,c);
+
+// c++ => cout <<"Sum of" << a << "and"<<b<<"is"<<c;
+
+
+
+// c    =>  scanf("%d", &a);
+
+// c++  => cin>>a;                        //  >> => Extraction Operator             // cin 
+
+//      => cin>>a>>b;
+
+
+
+// c => #include <stdio.h>
+
+// c++ => #include <iostream.h>
---
+++ C++_Language/04Condition.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a, b;
+	
+	cout<<"Enter value of a : ";
+	cin>>a;
+	cout<< "Enter value of b : ";
+	cin>>b;
+	
+	if(a>b){
+		cout<< "a is greater than b"<<endl;
+	}
+	else{
+		cout<< "b is greater than a"<<endl;
+	}
+	
+}
+
+
+
